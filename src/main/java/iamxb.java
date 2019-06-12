280060904b9521f71b30e2060b932c931e9384f4
rss <470971464@qq.com>
2019-06-12 20:41:38 +0800

    添加关于提交给Github的基本语法

--- src/main/java/iamxb.java
+++ src/main/java/iamxb.java
@@ -1,6 +1,12 @@
 public class iamxb {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("我是学霸君");
 
+    }
+
+    public void version()
+    {
+        System.out.println("感谢观看");
     }
 }
